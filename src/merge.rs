a48660a569e01374025d397f28d70cdf2955b343
yito88 <yuji@phact-columba.com>
2021-09-27 12:03:08 +0200

    add the leaf prefix

--- src/merge.rs
+++ src/merge.rs
@@ -16,13 +16,14 @@ pub fn merge<H: Hasher + Default>(lhs: &H256, rhs: &H256) -> H256 {
     hasher.finish()
 }
 
-/// hash_leaf = hash(key | value)
+/// hash_leaf = hash(prefix | key | value)
 /// zero value represent delete the key, this function return zero for zero value
 pub fn hash_leaf<H: Hasher + Default>(key: &H256, value: &H256) -> H256 {
     if value.is_zero() {
         return H256::zero();
     }
     let mut hasher = H::default();
+    hasher.write_h256(&H256::zero());
     hasher.write_h256(key);
     hasher.write_h256(value);
     hasher.finish()
